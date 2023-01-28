0d20ab2fd2dfe85350acaafdadd589990ad4703a
Alexander Kirilin <nerosashok@mail.ru>
2023-01-29 00:00:09 +0300

    Правка методов

--- 55/Program.cs
+++ 55/Program.cs
@@ -1,9 +1,9 @@
 ﻿int[,] GetArray(int rows, int columns, int minValue, int maxValue)                      //метод получения двумерного массива с рандомными значениями
 {
     int[,] result = new int[rows, columns];
-    for (int i = 0; i < columns; i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < rows; j++)
+        for (int j = 0; j < columns; j++)
         {
             result[i, j] = new Random().Next(minValue, maxValue + 1);
         }
@@ -13,9 +13,9 @@
 void PrintArray(int[,] inArray)                                                         //метод вывода двумерного массива с корректными отступами
 {
     Console.WriteLine();
-    for (int i = 0; i < inArray.GetLength(1); i++)
+    for (int i = 0; i < inArray.GetLength(0); i++)
     {
-        for (int j = 0; j < inArray.GetLength(0); j++)
+        for (int j = 0; j < inArray.GetLength(1); j++)
         {
             if (inArray[i, j] >= 0 && inArray[i, j] < 10) Console.Write($"     {inArray[i, j]}");
             if (inArray[i, j] >= 10 && inArray[i, j] < 100) Console.Write($"    {inArray[i, j]}");
